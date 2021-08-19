b0cd1aeef5f03177ff9605eadca20a530d48a525
caueguedes <caue.guedes91@gmail.com>
2021-08-18 23:09:50 -0300

    Add helpers
    helpers:
    - page_name (use translate to add title to page)
    - links (list of links to asideDrawer)
    - isEditing (set variable props to ReadOnly)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+  def page_name
+    translate("#{controller_name}.title")
+  end
+
+  def links
+    links = []
+    links << { text: 'Units', link: '/admin/units'}
+    links << { text: 'Bottles', link: '/admin/bottles'}
+    links << { text: 'Plans', link: '/admin/plans'} if current_user.admin?
+  end
+
+  def is_editing
+    action_name.in? ['new', 'create', 'update']
+  end
 end
