0e9d6489505bef154c5bf8e9de0a2b0bf77a0738
caueguedes <caue.guedes91@gmail.com>
2021-08-20 02:39:45 -0300

    sessions controller to add auth

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,16 +1,19 @@
 import React, { Suspense, lazy } from 'react';
 import { Router, Route, Switch } from "react-router-dom";
 
-import { Loading } from './pages'
-
 import { history } from "./utils";
 
 import { RestrictedRoute } from "./components";
 
+import { Loading } from './pages'
+
 const Home = lazy(() => import('./pages/Home'));
 const Login = lazy(() => import('./pages/Login'));
-const Register = lazy(() => import('./pages/Register'));
 const NotFound = lazy(() => import('./pages/NotFound'));
+const Pricing = lazy(() => import("./pages/Pricing"));
+const Register = lazy(() => import('./pages/Register'));
+const Units = lazy(() => import("./pages/Units"));
+
 
 const App = () => {
   return (
@@ -19,6 +22,8 @@ const App = () => {
         <Switch>
 
           <Route exact path="/" component={Home} />
+          <Route path="/pricing" component={Pricing} />
+          <Route path="/units" component={Units} />
 
           <RestrictedRoute path="/register" component={Register} />
           <RestrictedRoute path="/login" component={Login} />
