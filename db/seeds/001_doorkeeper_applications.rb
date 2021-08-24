592e69dde125eb86eb69f016c1d5a343b07692d6
caueguedes <caue.guedes91@gmail.com>
2021-08-24 11:27:04 -0300

    Set Default application for faster start up

--- db/seeds/001_doorkeeper_applications.rb
+++ db/seeds/001_doorkeeper_applications.rb
@@ -1,3 +1,4 @@
 if Doorkeeper::Application.count.zero?
-  Doorkeeper::Application.create(name: "Api clients", redirect_uri: "", scopes: "")
+  Doorkeeper::Application.create(name: "Api clients", redirect_uri: "", scopes: "",
+     uid: "f8R9N0AAkYtOfnVM_W9ZnLFa85uJjgbuPztMSP2mU4E", secret: "nYpmUxwythHSzB-7RXnamJ6ZSNirderMVI2AABn86m4")
 end
--- spec/factories/bottles.rb
+++ spec/factories/bottles.rb
@@ -4,6 +4,7 @@ FactoryBot.define do
     brand { Faker::Lorem.word }
     country { Faker::Address.country }
     bottled { Faker::Number.between(from: 1501, to: DateTime.now.year) }
+    image { Faker::LoremFlickr.image }
     # sequence :type, %w[red white rose sparkling fortified].cycle
     # sequence :coloring, %w[red white rose].cycle
     description { Faker::Lorem.paragraph }
